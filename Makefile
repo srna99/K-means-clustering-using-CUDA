c3d1851ed5776c874e22a7eb08780866df5d9307
Serena Cheng <serena.ch1999@gmail.com>
2021-10-22 23:27:14 -0400

    Add libarff and partitioned datasets

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 todo: kmeans
 kmeans: kmeans.cpp
-	g++ -o kmeans kmeans.cpp
+	g++ -o kmeans kmeans.cpp -Ilibarff libarff/arff_attr.cpp libarff/arff_data.cpp libarff/arff_instance.cpp libarff/arff_lexer.cpp libarff/arff_parser.cpp libarff/arff_scanner.cpp libarff/arff_token.cpp libarff/arff_utils.cpp libarff/arff_value.cpp
 clean:
 	rm kmeans
--- kmeans.cpp
+++ kmeans.cpp
@@ -4,10 +4,18 @@
 // #include <float.h>
 // #include <math.h>
 #include <iostream>
+#include "libarff/arff_parser.h"
+#include "libarff/arff_data.h"
 
 using namespace std;
 
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[]) {
+    if(argc != 3)
+    {
+        cout << "Usage: ./kmeans datasets/dataset.arff k" << endl;
+        exit(0);
+    }
+
+    int k = strtol(argv[2], NULL, 10);
 }
