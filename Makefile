9f1f108521e8269973a49094c7272ecf08aa2e5e
Serena Cheng <serena.ch1999@gmail.com>
2021-10-22 22:55:53 -0400

    Add dataset, makefile, and kmeans.cpp

---
+++ Makefile
@@ -0,0 +1,5 @@
+todo: kmeans
+kmeans: kmeans.cpp
+	g++ -o kmeans kmeans.cpp
+clean:
+	rm kmeans
---
+++ kmeans.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+// #include <stdint.h>
+// #include <float.h>
+// #include <math.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+    
+}
